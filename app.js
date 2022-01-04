da71084929c221f169e7703ef70d8df5cae7f5fd
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2022-01-04 20:40:15 +0300

    chalk and yargs

---
+++ app.js
@@ -0,0 +1,6 @@
+const decrease = require("./notes");
+const yargs = require("yargs");
+
+const chalk = require("chalk");
+
+console.log(process.argv);
--- notes-app/app.js
+++
@@ -1,6 +0,0 @@
-const chalk = require("chalk");
-const decrease = require("./notes");
-
-const final = decrease();
-
-console.log("Success");
